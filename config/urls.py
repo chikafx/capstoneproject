0c1cd7fc41d8f932e3ce947bc77dab7d42fd5e50
chikafx <kingsobi70@yahoo.com>
2024-11-30 18:19:23 +0100

    swagger added to my code

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My Documentation",
+      default_version='v1',
+      description="This is the doc to my project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="kingsobi70@yahoo.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0)),
     path('admin/', admin.site.urls),
     path('', include('inventory.urls'))
 ]
